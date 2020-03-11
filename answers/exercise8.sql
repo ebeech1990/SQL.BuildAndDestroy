30ce1209aba8be907fbeb5dc32aea1e6ced8d6e1
ebeech <31962702+ebeech1990@users.noreply.github.com>
2020-03-11 17:49:13 -0400

    Update exercise8.sql

---
+++ answers/exercise8.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Students(
+	StudentName VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(255),
+	Zipcode VARCHAR(10),
+	Country VARCHAR(50)
+);
+
+INSERT INTO Students
+(StudentName, Address, City, Zipcode, Country)
+VALUES('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
